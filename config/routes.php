7d6486d917adba5b303eb88a6856b9e6786573ee
klesio <klesio.dev@gmail.com>
2024-11-25 02:32:12 -0300

    Revert "feat(main): testes e rodando projeto"
    This reverts commit ff697440dc8b1f74c59ee2e9a487e35749c7aa70.

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Controllers\HomeController;
-use App\Controllers\LoginController;
 use Core\Router\Route;
 
 // Authentication
 Route::get('/', [HomeController::class, 'index'])->name('root');
-Route::get('/login', [LoginController::class, 'index'])->name('login');
