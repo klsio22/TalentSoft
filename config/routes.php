60d23985bafae37beebcb04646409c7df80d1878
klesio <klesio.dev@gmail.com>
2024-11-25 03:22:17 -0300

    feat(main): testes e rodando projeto

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\LoginController;
 use Core\Router\Route;
 
 // Authentication
 Route::get('/', [HomeController::class, 'index'])->name('root');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
