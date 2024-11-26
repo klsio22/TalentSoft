0188e864d27d0563d3ec038d5a668be89b133b53
klesio <klesio.dev@gmail.com>
2024-11-26 01:01:26 -0300

    feat(user-authentication): created login screen and user authentication on login

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,31 @@ namespace App\Models;
 
 class User
 {
+    public int $id;
+    public string $email;
+    public string $password;
+
     public static function attempt(array $credentials): ?self
     {
-        if ($credentials['username'] === 'validUser' && $credentials['password'] === 'validPassword') {
-            return new self();
+        if ($credentials['email'] === 'validUser@example.com' && $credentials['password'] === 'validPassword') {
+            $user = new self();
+            $user->id = 1; // Defina um ID fictício para o usuário
+            $user->email = $credentials['email'];
+            $user->password = $credentials['password'];
+            return $user;
+        }
+        return null;
+    }
+
+    public static function findById(int $id): ?self
+    {
+        // Implementação fictícia para encontrar um usuário pelo ID
+        if ($id === 1) {
+            $user = new self();
+            $user->id = 1;
+            $user->email = 'validUser@example.com';
+            $user->password = 'validPassword';
+            return $user;
         }
         return null;
     }
