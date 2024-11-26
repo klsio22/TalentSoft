7feb4f43756148f7d7a4242990c90163c8ace108
klesio <klesio.dev@gmail.com>
2024-11-26 20:05:49 -0300

    feat(user-authentication): login section validation

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -10,36 +10,39 @@ use App\Models\User;
 
 class LoginController extends Controller
 {
-  public function showLoginForm(): void
-  {
-    $this->render('auth/login');
-  }
-
-  public function login(Request $request): void
-  {
-    $credentials = $request->only(['email', 'password']);
-    $user = User::attempt($credentials);
-
-    if ($user) {
-      Auth::login($user);
-      FlashMessage::success('Login realizado com sucesso');
-      $this->redirectTo(route('home'));
-    } else {
-      FlashMessage::danger('Credenciais inválidas');
-      $this->redirectTo(route('users.login'));
+    public function showLoginForm(): void
+    {
+        if (Auth::check()) {
+            $this->redirectTo(route('home'));
+        }
+        $this->render('auth/login');
     }
-  }
 
-  public function logout(): void
-  {
-    Auth::logout();
-    FlashMessage::success('Logout realizado com sucesso');
-    $this->redirectTo(route('root'));
-  }
+    public function login(Request $request): void
+    {
+        $credentials = $request->only(['email', 'password']);
+        $user = User::attempt($credentials);
 
-  protected function redirectTo(string $location): void
-  {
-    header('Location: ' . $location);
-    exit;
-  }
+        if ($user) {
+            Auth::login($user);
+            FlashMessage::success('Login realizado com sucesso');
+            $this->redirectTo(route('home'));
+        } else {
+            FlashMessage::danger('Credenciais inválidas');
+            $this->redirectTo(route('users.login'));
+        }
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        FlashMessage::success('Logout realizado com sucesso');
+        $this->redirectTo(route('root'));
+    }
+
+    protected function redirectTo(string $location): void
+    {
+        header('Location: ' . $location);
+        exit;
+    }
 }
